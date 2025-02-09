f5ff625af6ad146ca2d22cddadaf865fccbd050c
김지혜 <ria9810@gmail.com>
2025-02-10 00:58:34 +0900

    15일차 과제 업로드

---
+++ quiz/day15/src/components/ContactEditor.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./ContactEditor.css";
+
+export default function ContactEditor({ onCreateContact }) {
+  const [state, setState] = useState({
+    name: "",
+    contact: "",
+  });
+
+  const onChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = () => {
+    if (state.name === "" || state.contact === "") {
+      alert("이름과 연락처를 입력해주세요");
+      return;
+    }
+    onCreateContact(state.name, state.contact);
+
+    setState({
+      name: "",
+      contact: "",
+    });
+  };
+
+  return (
+    <div className="ContactEditor">
+      <div className="title">Add Contact</div>
+      <div className="input_wrapper">
+        <input
+          name="name"
+          value={state.name}
+          onChange={onChangeState}
+          className="name"
+          placeholder="이름 ..."
+        />
+        <input
+          name="contact"
+          value={state.contact}
+          onChange={onChangeState}
+          className="contact"
+          placeholder="연락처(이메일) ..."
+        />
+      </div>
+      <button onClick={onSubmit}>Add</button>
+    </div>
+  );
+}
